eecfa1783d3afec313b201fc431d4ae49c86f73f
ivanfsilva <ivanfs27@gmail.com>
2019-11-06 21:59:37 -0300

    Projeto criado

---
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ivanfsilva.pedidowebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoWebApiApplication.class, args);
+	}
+
+}
