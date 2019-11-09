0f12ad8088e4eb2477e8ae4993176163a8104e64
ivanfsilva <ivanfs27@gmail.com>
2019-11-09 10:28:07 -0300

    Criando operação de instanciação

--- src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
@@ -1,13 +1,33 @@
 package br.com.ivanfsilva.pedidowebapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PedidoWebApiApplication {
+import br.com.ivanfsilva.pedidowebapi.domain.Categoria;
+import br.com.ivanfsilva.pedidowebapi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PedidoWebApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoWebApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
