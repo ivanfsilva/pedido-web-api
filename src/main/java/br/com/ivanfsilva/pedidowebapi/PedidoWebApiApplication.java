4cf8655efdce059c8095613821751935749d29ea
ivanfsilva <ivanfs27@gmail.com>
2019-11-09 12:10:11 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.ivanfsilva.pedidowebapi.domain.Categoria;
+import br.com.ivanfsilva.pedidowebapi.domain.Produto;
 import br.com.ivanfsilva.pedidowebapi.repositories.CategoriaRepository;
+import br.com.ivanfsilva.pedidowebapi.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PedidoWebApiApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoWebApiApplication.class, args);
@@ -26,7 +31,21 @@ public class PedidoWebApiApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 
