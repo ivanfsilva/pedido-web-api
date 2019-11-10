2c15af606b4146de8c2fee041493d48c289c98ee
ivanfsilva <ivanfs27@gmail.com>
2019-11-10 12:27:04 -0300

    Deletando uma categoria com verbo DELETE

--- src/main/java/br/com/ivanfsilva/pedidowebapi/services/CategoriaService.java
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/services/CategoriaService.java
@@ -3,12 +3,13 @@ package br.com.ivanfsilva.pedidowebapi.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.ivanfsilva.pedidowebapi.domain.Categoria;
 import br.com.ivanfsilva.pedidowebapi.repositories.CategoriaRepository;
+import br.com.ivanfsilva.pedidowebapi.services.exceptions.DataIntegrityException;
 import br.com.ivanfsilva.pedidowebapi.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class CategoriaService {
@@ -30,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
