62764cc5018e2461d552970eef44ecaaf16b8064
ivanfsilva <ivanfs27@gmail.com>
2019-11-10 00:09:52 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.ivanfsilva.pedidowebapi;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.ivanfsilva.pedidowebapi.domain.Cidade;
 import br.com.ivanfsilva.pedidowebapi.domain.Cliente;
 import br.com.ivanfsilva.pedidowebapi.domain.Endereco;
 import br.com.ivanfsilva.pedidowebapi.domain.Estado;
+import br.com.ivanfsilva.pedidowebapi.domain.Pagamento;
+import br.com.ivanfsilva.pedidowebapi.domain.PagamentoComBoleto;
+import br.com.ivanfsilva.pedidowebapi.domain.PagamentoComCartao;
+import br.com.ivanfsilva.pedidowebapi.domain.Pedido;
 import br.com.ivanfsilva.pedidowebapi.domain.Produto;
+import br.com.ivanfsilva.pedidowebapi.domain.enums.EstadoPagamento;
 import br.com.ivanfsilva.pedidowebapi.domain.enums.TipoCliente;
 import br.com.ivanfsilva.pedidowebapi.repositories.CategoriaRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.CidadeRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.ClienteRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.EnderecoRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.EstadoRepository;
+import br.com.ivanfsilva.pedidowebapi.repositories.PagamentoRepository;
+import br.com.ivanfsilva.pedidowebapi.repositories.PedidoRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class PedidoWebApiApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoWebApiApplication.class, args);
@@ -84,6 +96,23 @@ public class PedidoWebApiApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2019 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2019 19:35"), cli1, e2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2019 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
 	}
 
