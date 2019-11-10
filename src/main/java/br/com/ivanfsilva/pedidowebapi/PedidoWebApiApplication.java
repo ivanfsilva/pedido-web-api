426d525365c117fcb4ce5d61a49f778df8a0fcc1
ivanfsilva <ivanfs27@gmail.com>
2019-11-10 01:12:12 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/PedidoWebApiApplication.java
@@ -13,6 +13,7 @@ import br.com.ivanfsilva.pedidowebapi.domain.Cidade;
 import br.com.ivanfsilva.pedidowebapi.domain.Cliente;
 import br.com.ivanfsilva.pedidowebapi.domain.Endereco;
 import br.com.ivanfsilva.pedidowebapi.domain.Estado;
+import br.com.ivanfsilva.pedidowebapi.domain.ItemPedido;
 import br.com.ivanfsilva.pedidowebapi.domain.Pagamento;
 import br.com.ivanfsilva.pedidowebapi.domain.PagamentoComBoleto;
 import br.com.ivanfsilva.pedidowebapi.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.ivanfsilva.pedidowebapi.repositories.CidadeRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.ClienteRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.EnderecoRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.EstadoRepository;
+import br.com.ivanfsilva.pedidowebapi.repositories.ItemPedidoRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.PagamentoRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.PedidoRepository;
 import br.com.ivanfsilva.pedidowebapi.repositories.ProdutoRepository;
@@ -48,7 +50,9 @@ public class PedidoWebApiApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoWebApiApplication.class, args);
 	}
@@ -85,34 +89,48 @@ public class PedidoWebApiApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "23423434", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("1213", "32234"));
-		
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "234234", cli1, c1);
 		Endereco e2 = new Endereco(null, "Av. Matos", "105", "Sala 800", "Centro", "6747567", cli1, c2);
-		
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		
+
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2019 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2019 19:35"), cli1, e2);
-		
+
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
+
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		
+
 		ped1.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2019 00:00"), null);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2019 00:00"),
+				null);
 		ped2.setPagamento(pagto2);
-		
+
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
--- src/main/java/br/com/ivanfsilva/pedidowebapi/domain/Produto.java
+++ src/main/java/br/com/ivanfsilva/pedidowebapi/domain/Produto.java
@@ -2,7 +2,9 @@ package br.com.ivanfsilva.pedidowebapi.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -29,10 +32,19 @@ public class Produto implements Serializable {
 
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name = "produto_categoria", 
-		joinColumns = @JoinColumn(name = "produto_id"), 
-		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	public Produto() {
 
@@ -43,6 +55,15 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
